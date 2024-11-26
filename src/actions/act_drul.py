985d1b56b5cc917ea046ff797ca8274c34ed1f4b
Michael Riedl <michael@michaelriedl.com>
2024-11-25 22:29:36 -0500

    Updated DRUL action

--- src/actions/act_drul.py
+++ src/actions/act_drul.py
@@ -5,15 +5,16 @@ import jax.numpy as jnp
 def act_drul(rng_key: jax.Array, obs: jax.Array, mask: jax.Array):
     """
     Ignore observation and prioritize actions in the order of down, right, up, left.
+    This function does not work with batched inputs.
 
     Parameters
     ----------
     rng_key : jax.Array
         Random key.
     obs : jax.Array
-        Observation.
+        Observation. The shape of the observation should be (4, 4, 31).
     mask : jax.Array
-        Legal action mask.
+        Legal action mask. The mask should be a one-hot vector of shape (4,).
 
     Returns
     -------
@@ -25,9 +26,9 @@ def act_drul(rng_key: jax.Array, obs: jax.Array, mask: jax.Array):
     action_order = jnp.array([3, 2, 1, 0], dtype=jnp.int32)
 
     # Reorder the mask
-    mask = mask[:, action_order]
+    mask = mask[action_order]
 
     # Choose the first legal action
-    action = action_order[mask.argmax(axis=-1)]
+    action = action_order[mask.argmax()]
 
     return action
