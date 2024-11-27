14fa7bf6bea16a719a05f50ed2f083d961f1cffe
Michael Riedl <michael@michaelriedl.com>
2024-11-26 21:04:01 -0500

    Updated random action function

--- src/actions/act_randomly.py
+++ src/actions/act_randomly.py
@@ -4,16 +4,17 @@ import jax.numpy as jnp
 
 def act_randomly(rng_key: jax.Array, obs: jax.Array, mask: jax.Array):
     """
-    Ignore observation and choose randomly from legal actions.
+    Ignore observation and choose randomly from legal actions. This function does not
+    work with batched inputs.
 
     Parameters
     ----------
     rng_key : jax.Array
         Random key.
     obs : jax.Array
-        Observation.
+        Observation. The shape of the observation should be (4, 4, 31).
     mask : jax.Array
-        Legal action mask.
+        Legal action mask. The mask should be a one-hot vector of shape (4,).
 
     Returns
     -------
@@ -21,7 +22,7 @@ def act_randomly(rng_key: jax.Array, obs: jax.Array, mask: jax.Array):
         Randomly chosen action.
 
     """
-    probs = mask / mask.sum(axis=-1, keepdims=True)
+    probs = mask / mask.sum()
     logits = jnp.maximum(jnp.log(probs), jnp.finfo(probs.dtype).min)
 
-    return jax.random.categorical(rng_key, logits=logits, axis=-1)
+    return jax.random.categorical(rng_key, logits=logits)
