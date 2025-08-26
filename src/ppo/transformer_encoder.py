91980672cf7c799ee0e02715514d33f929149d10
Michael Riedl <michael@michaelriedl.com>
2025-08-25 21:12:04 -0400

    Added 2D positional encoding

--- src/ppo/transformer_encoder.py
+++ src/ppo/transformer_encoder.py
@@ -1,5 +1,95 @@
+import numpy as np
 import torch
 import torch.nn as nn
+
+BOARD_SIZE = 4
+
+
+def get_emb(sin_inp):
+    """
+    Gets a base embedding for one dimension with sin and cos intertwined
+    """
+    emb = torch.stack((sin_inp.sin(), sin_inp.cos()), dim=-1)
+    return torch.flatten(emb, -2, -1)
+
+
+class PositionalEncoding2D(nn.Module):
+    def __init__(self, x_size, y_size, channels, dropout=0.1, dtype_override=None):
+        """
+        :param channels: The last dimension of the tensor you want to apply pos emb to.
+        :param dtype_override: If set, overrides the dtype of the output embedding.
+        """
+        super(PositionalEncoding2D, self).__init__()
+        self.org_channels = channels
+        channels = int(np.ceil(channels / 4) * 2)
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
+        self.register_buffer("inv_freq", inv_freq)
+        self.register_buffer("cached_penc", None, persistent=False)
+        self.dtype_override = dtype_override
+        self.channels = channels
+
+        # Create the dropout
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Create the positional encoding matrix
+        pos_x = torch.arange(x_size, dtype=self.inv_freq.dtype)
+        pos_y = torch.arange(y_size, dtype=self.inv_freq.dtype)
+        sin_inp_x = torch.einsum("i,j->ij", pos_x, self.inv_freq)
+        sin_inp_y = torch.einsum("i,j->ij", pos_y, self.inv_freq)
+        emb_x = get_emb(sin_inp_x).unsqueeze(1)
+        emb_y = get_emb(sin_inp_y)
+        emb = torch.zeros(
+            (x_size, y_size, self.channels * 2),
+            dtype=torch.float,
+        )
+        emb[:, :, : self.channels] = emb_x
+        emb[:, :, self.channels : 2 * self.channels] = emb_y
+        emb = emb.unsqueeze(0)
+        self.register_buffer("pe", emb)
+
+    def forward(self, tensor):
+        """
+        :param tensor: A 4d tensor of size (batch_size, x, y, ch)
+        :return: Tensor of the same size with the positional encoding added
+        """
+        tensor = tensor + self.pe[:, : tensor.size(1), : tensor.size(2)]
+
+        return self.dropout(tensor)
+
+    def forward_flat(self, tensor: torch.Tensor) -> torch.Tensor:
+        """
+        :param tensor: A 3d tensor of size (batch_size, xy, ch). It is assumed
+            that x and y are the same x_size and y_size as the embedding.
+        :return: Tensor of the same size with the positional encoding added
+        """
+        pe = self.pe.flatten(1, 2)
+        tensor = tensor + pe
+        return self.dropout(tensor)
+
+    def forward_with_inds(self, x: torch.Tensor, inds: torch.Tensor) -> torch.Tensor:
+        """
+        Perform a forward pass of the module.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            The input tensor to apply the positional encoding to. The shape
+            should be (batch_size, seq_len, d_model).
+        inds : torch.Tensor
+            The indices of the input tensor. The shape should be (batch_size, seq_len).
+
+        Returns
+        -------
+        torch.Tensor
+            The resulting tensor after applying the positional encoding to the
+            input. The shape is the same as the input.
+
+        """
+        # Flatten the positional encoding
+        pe = self.pe.flatten(1, 2)
+        x = x + pe[0, inds]
+
+        return self.dropout(x)
 
 
 class TransformerEncoder(nn.Module):
@@ -38,6 +128,10 @@ class TransformerEncoder(nn.Module):
         self.num_layers = num_layers
         self.dim_feedforward = dim_feedforward
         self.dropout = dropout
+        # Create the positional encoding
+        self.positional_encoding = PositionalEncoding2D(
+            BOARD_SIZE, BOARD_SIZE, d_model=self.d_model, dropout=self.dropout
+        )
         # Create the transformer encoder
         self.encoder = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(
@@ -64,4 +158,4 @@ class TransformerEncoder(nn.Module):
         torch.Tensor
             The output tensor with shape (batch_size, seq_len, d_model).
         """
-        return self.encoder(src)
+        return self.encoder(self.positional_encoding(src))
